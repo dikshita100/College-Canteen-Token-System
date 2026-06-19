4cd543e28d4a53212e3cc8a9a9b4d1b678279b83
dikshita100 <dikshitasiripireddy@gmail.com>
2026-06-19 18:52:27 +0530

    Login1.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,56 +1,37 @@
-import React, { useState } from 'react'
+import { useState } from "react"
 
 const Login = () => {
 
-    var [username, setUsername] = useState("");
-    var [password, setPassword] = useState("");
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
 
-    function handleUsername(e) {
-        setUsername(e.target.value);
-    }
+  return (
+    <div className="login">
 
-    function handlePassword(e) {
-        setPassword(e.target.value);
-    }
+      <div className="loginBox">
 
-    var message = "";
+        <h2>Login</h2>
 
-    if (username == "" || password == "")
-        message = "";
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+        <br/>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+        <br/>
+        <button>Login</button>
 
-    else if (password.length < 5)
-        message = "Password Length Must Be Greater Than 5";
+      </div>
 
-    else
-        message = "Validation Success";
-
-    return (
-        <div>
-          <h2>Login</h2>
-          <br/>
-          <label>Enter Username   </label> 
-            <input
-                type="text"
-                value={username}
-                placeholder="username"
-                onChange={handleUsername}
-            />
-            <br />
-            <label>Enter Password   </label>
-                <input
-                type="password"
-                value={password}
-                placeholder="password"
-                onChange={handlePassword}
-            />
-
-
-            <p>{message}</p>
-            <br/>
-            <button type="submit">Submit</button>
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Login
